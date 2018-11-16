27180f58049bf3cc338689213df848b5af57e01d
Evelyn LaTour <evelyn.a.latour@gmail.com>
2018-11-16 16:10:21 -0600

    helper functions for lazy load & popups

--- quick-scrape/file-rename.js
+++ quick-scrape/file-rename.js
@@ -1,18 +1,20 @@
 /* eslint no-restricted-syntax: 0 */
+/* eslint no-await-in-loop: 0 */
 
 const fs = require(`fs`);
+const colors = require(`colors`);
+
 
 const renameFiles = async (path, baseFileName, fileStartNum) => {
   const imageFiles = fs.readdirSync(path);
-  console.log(imageFiles.length);
   let fileNum = fileStartNum;
   for (const image of imageFiles) {
     await fs.rename(`${path}/${image}`, `${path}/${baseFileName}-${fileNum}.jpg`, (err) => {
       if (err) throw err;
     });
-    fileNum++;
+    fileNum += 1;
   }
-  console.log(`done renaming`);
+  console.log(`done renaming ${imageFiles.length} images`.bold.green);
 };
 
-renameFiles(`${__dirname}/../scraped-images/short-sleeve`, `short-sleeve`, 36);
+renameFiles(`${__dirname}/../scraped-images/long-sleeve`, `long-sleeve`, 279);
--- quick-scrape/main.js
+++ quick-scrape/main.js
@@ -7,46 +7,56 @@ const puppeteer = require(`puppeteer`);
 const colors = require(`colors`);
 const fs = require(`fs`);
 const axios = require(`axios`);
+const { saksOff5th, carbon38, f21 } = require(`./scrape-urls`);
+
+const placeholderUrl = `https://sspride.org/wp-content/uploads/2017/03/image-placeholder-500x500.jpg`;
 
 const downloadImage = async (urlArray, baseFileName, startingNum) => {
-  console.log(`downloading images locally...`.bold.yellow);
-  let firstFileNum = startingNum;
-  for (const url of urlArray) {
-    const filePath = `${__dirname}/../scraped-images/${baseFileName}/${baseFileName}-${firstFileNum}.jpg`;
-    const { data } = await axios.get(url, { responseType: `stream` });
-    data.pipe(fs.createWriteStream(filePath));
-    await new Promise((resolve, reject) => {
-      data.on(`end`, resolve);
-      data.on(`error`, reject);
-    });
-    firstFileNum++;
+  try {
+    console.log(`downloading images locally...`.bold.yellow);
+    let firstFileNum = startingNum;
+    for (let url of urlArray) {
+      if (!url.startsWith(`http` || `https`)) url = placeholderUrl; // in case a non-image gets through
+      const filePath = `${__dirname}/../scraped-images/${baseFileName}/${baseFileName}-${firstFileNum}.jpg`;
+      const { data } = await axios.get(url, { responseType: `stream` });
+      data.pipe(fs.createWriteStream(filePath));
+      await new Promise((resolve, reject) => {
+        data.on(`end`, resolve);
+        data.on(`error`, reject);
+      });
+      firstFileNum += 1;
+    }
+    console.log(`done downloading images!`.bold.green);
+  } catch (err) {
+    console.log(err);
   }
-  console.log(`done downloading images!`.bold.green);
 };
+
 
 const runScrape = async (url, baseFileName, startingNum) => {
   try {
-    const browser = await puppeteer.launch({ headless: true, timeout: 0 });
+    const browser = await puppeteer.launch({ headless: false, timeout: 0 });
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: `domcontentloaded` });
+    await page.goto(url, { waitUntil: `networkidle0` });
     await page.setViewport({ width: 1000, height: 800 });
 
-    // Select option to show 180 images (or other num available)
-    // await Promise.all([page.click(`#pc-top > div:nth-child(2) > a`), page.waitForNavigation()]);
+    /** call a helper func here if needed * */
 
-    // Grab each img src on the page and push to array
+
     console.log(`grabbing image urls from site...`.bold.yellow);
-
     const getImageUrls = await page.evaluate(async () => {
-      const images = await document.querySelectorAll(`div.image-container-large > a img`); // .map(img => img.getAttribute(`src`));
+      const images = await document.querySelectorAll(`.p_item div:nth-child(2) > a img`);
       const imageSources = [];
       for (const image of images) {
-        const imgSrc = image.getAttribute(`src`);
-        imageSources.push(imgSrc);
+        const imgAttr1 = image.getAttribute(`src`);
+        // const imgAttr2 = image.getAttribute(`data-hover-image`);
+        imageSources.push(imgAttr1);
       }
       return imageSources;
     });
     console.log(`done getting image urls!`.bold.green);
+    console.log(getImageUrls);
+    console.log(`There are ${getImageUrls.length} images`);
     downloadImage(getImageUrls, baseFileName, startingNum);
     browser.close();
   } catch (err) {
@@ -55,9 +65,42 @@ const runScrape = async (url, baseFileName, startingNum) => {
   }
 };
 
-const sweatersLongSleeveUrl = `https://www.saksoff5th.com/Women/Apparel/Sweaters/shop/_/N-4ztf3j/Ne-6ja3nn`;
-const longSleeveUrl = `https://www.saksoff5th.com/Women/Apparel/Tops/shop/_/N-4zteytZ1z13xsi?Ne=395181059&FOLDER%3C%3Efolder_id=2534374302023685`;
-const shortSleeveUrl = `https://www.saksoff5th.com/Women/Apparel/Tops/T-Shirts-and-Tanks/shop/_/N-4zti47Z1z13xs3?Ne=395181059&Ne=395181059&FOLDER%3C%3Efolder_id=2534374302027767`;
-const activewearShortSleeveUrl = `https://www.saksoff5th.com/Women/Apparel/Activewear/shop/_/N-4ztf0mZ1z13xs3/Ne-6ja3nn?FOLDER%3C%3Efolder_id=2534374302023750#`;
 
-runScrape(activewearShortSleeveUrl, `short-sleeve`, 600);
+runScrape(f21.longSleeveUrl, `long-sleeve`, 700);
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Helper Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+async function carbon38Helper(page) {
+  // Scroll more images into view & dismiss popup for Carbon38
+  page.evaluate(() => {
+    window.scrollBy(0, document.body.scrollHeight, { behavior: `smooth` }); // go to bottom
+  });
+  await page.waitFor(2000); // wait for page load more images
+  page.evaluate(() => { // force pop-up to appear
+    window.scrollBy(0, -200, { behavior: `smooth` });
+  });
+  await page.waitFor(4000); // wait for pop up to render on dom
+  page.click(`#popup-subscribe-first > div > div > p`); // dismiss pop-up
+  await page.waitFor(3000);
+}
+
+async function saksOff5thHelper(page) {
+  // Select option to show 180 images (or other num available) on saksoff5th
+  await Promise.all([page.click(`#pc-top > div:nth-child(2) > a`), page.waitForNavigation()]);
+}
+
+async function f21Helper(page) {
+  page.evaluate(() => {
+    window.scrollBy(0, document.body.scrollHeight, { behavior: `smooth` });
+    window.scrollBy(0, -1000, { behavior: `smooth` });
+  });
+  await page.waitFor(4000);
+  page.click(`#bx-element-743676-nENzD9J > button`); // dismiss pop-up
+  await page.waitFor(4000);
+  page.evaluate(() => {
+    window.scrollBy(0, 500, { behavior: `smooth` });
+  });
+  await page.waitFor(4000);
+}
